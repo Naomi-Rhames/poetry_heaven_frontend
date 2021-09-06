4df38b0cde5bb829b0a9353e4e7f24f6f226637c
Naomi-Rhames <naomirhames2020@gmail.com>
2021-09-06 15:16:59 -0500

    refactor render

--- index.js
+++ index.js
@@ -51,20 +51,7 @@ function getPoem() {
     fetch(myAPI)
     .then(response => response.json())
     .then(poem => { poem.data.forEach(poem => { // we are using an arrow function because we are iterating through my API
-      const poemsMarkup = `
-        <div data-id=${poem.id}>
-          <img
-          src=${poem.attributes.image_url}
-          height="200" width="250">
-          <h3>${poem.attributes.title}</h3>
-          <p> ${poem.attributes.author}</p>
-          <p>${poem.attributes.stanza}</p>
-          <button data-id=${poem.id}>edit</button>
-          </div>
-          <br></br>`;
-
-
-      document.querySelector('#poem-container').innerHTML += poemsMarkup // we are appending it and updating thr inner HTML and appending that #poem-container div that we created in the index.html file
+      render(poem)// we are appending it and updating thr inner HTML and appending that #poem-container div that we created in the index.html file
       })   
   })
 
@@ -72,3 +59,20 @@ function getPoem() {
 
 }
 
+function render(poem){
+  const poemsMarkup = `
+  <div data-id=${poem.id}>
+    <img
+    src=${poem.attributes.image_url}
+    height="200" width="250">
+    <h3>${poem.attributes.title}</h3>
+    <p> ${poem.attributes.author}</p>
+    <p>${poem.attributes.stanza}</p>
+    <button data-id=${poem.id}>edit</button>
+    </div>
+    <br></br>`;
+
+
+  document.querySelector('#poem-container').innerHTML += poemsMarkup 
+  
+}
