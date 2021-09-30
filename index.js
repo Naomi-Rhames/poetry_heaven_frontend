bcc5ca9fb3479f385625cd2724524c9aec01f609
Naomi-Rhames <naomirhames2020@gmail.com>
2021-09-30 14:28:51 -0500

    deleted commented  notes

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 
 const myAPI = "http://127.0.0.1:3000/api/v1/poems"
 
-// we use inner text to render the html property of emelent innerface represet the rendered text
-// The JavaScript innerHTML property sets the HTML contents of an element on a web page
-
- // A fetch always returns a promise
- // A promise is like a uber app, it can reject or pend or forfill a request from the fetch and its asycornous object (resturaunt expample with waitress)
- // Syn object is code running at the same time but diffrent tasks
-
- // Asyc arr important because you don't want the user to wait to get all the information from the fetch call to continue to populate the DOM
 
 function postFetch(image_url, title, genre, author, stanza, category_id){
-  const bodyData = {image_url, title, genre, author, stanza, category_id} /// only with the scope '"BodyData can't change"
+  const bodyData = {image_url, title, genre, author, stanza, category_id} 
   fetch(myAPI, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify(bodyData)
   })
-  .then(response => response.json()) //  reutrns the data if the promise if forfilled
+  .then(response => response.json()) 
   .then(poem => { 
     const newPoems = new Poem(poem.data.id, poem.data.attributes)
       document.querySelector('#poem-container').innerHTML += newPoems.renderPoemCard();
-
-     
   })
  
 }
 
-function updateFormHandler(e){ // this "e" represents the event listener  it allows if you pass it in if you need it  or don't pass it in if you don't need it 
-  e.preventDefault(); // I am preventing the deafualt beahavior button from refreahing the page
+function updateFormHandler(e){
+  e.preventDefault(); 
   const id = parseInt(e.target.id); 
   const poem = Poem.findById(id)
   const title = e.target.querySelector('#input-title').value;
@@ -52,7 +42,7 @@ function patchPoem(title, genre, author, stanza, image_url, category_id, poem){
   })
   .then(res => res.json())
   .then(updatePoem => { 
-    let poem = Poem.findById(updatePoem.data.id); // we are giving an id so we can find that the poem instance
+    let poem = Poem.findById(updatePoem.data.id);
     poem.update(updatePoem.data.attributes); 
     document.querySelector('#poem-container').innerHTML = '';
     Poem.all.forEach(poem => document.querySelector('#poem-container').innerHTML += poem.renderPoemCard());
@@ -108,7 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-function createFormHandler(e){ // this prevent Deafult Behavior
+function createFormHandler(e){ 
   e.preventDefault()
   const imageInput = document.querySelector('#input-url').value
   const titleInput = document.querySelector('#input-title').value
@@ -122,18 +112,16 @@ function createFormHandler(e){ // this prevent Deafult Behavior
 function getPoem() {
     fetch(myAPI)
     .then(response => response.json())
-    .then(poem => { poem.data.forEach(poem => { // we are using an arrow function because we are iterating through my API
+    .then(poem => { poem.data.forEach(poem => {
      
      let displayPoem = new Poem(poem.id, poem.attributes)
-     document.querySelector('#poem-container').innerHTML += displayPoem.renderPoemCard() // When I am invoking the function it would just call it when this line is read rather than with the paranthesis I am only calling it when the event listener happens
+     document.querySelector('#poem-container').innerHTML += displayPoem.renderPoemCard() 
       }) 
 
   
       
   })
   
-
- // A callback function is a function that's within another function and that this function only runs when this funtion is ran
 }
 // const search = document.querySelector("#search")
 
@@ -143,8 +131,7 @@ function getPoem() {
 //     let query = document.getElementById("search-bar").value
 //       fetch(`http://127.0.0.1:3000/api/v1/search/${query}`)
 //       .then(res => res.json())
-//       .then(poems => console.log()
-//        
+//       .then(poems => console.log(poems)
      
 //       })
 //     })
--- poem.js
+++ poem.js
@@ -1,4 +1,5 @@
 class Poem {
+
     constructor(poem, poemAttributes){
         this.id = poem
         this.title = poemAttributes.title
@@ -11,54 +12,44 @@ class Poem {
     } 
 
     static findById(id){
-        return this.all.find(poem => { // which goes through the Poems array and pretty much finds that instance id if that matches the actual id that it's looking for
+        return this.all.find(poem => { 
           return  poem.id == id});
     }
 
     renderUpdateForm(){
         return `
-        <form id="${this.id}">
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Edit Title:</label>
-          <input type="text" class="form-control" id='input-title' value="${this.title}"aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">Speak creativly. Work your magic.</div>
-        </div>
-        <div>
-            <label for="exampleInputEmail1" class="form-label">Edit Genre</label>
-            <input type="text" value="${this.genre}"class="form-control" id='input-genre' aria-describedby="emailHelp">
-           
-        </div>
-        <label for="exampleInputEmail1" class="form-label">Edit Image:</label>
-        <input type="text" value="${this.image_url}"class="form-control" id='input-url' aria-describedby="emailHelp">
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Edit Stanza:</label>
-          <textarea class="form-control" value="${this.stanza}" id='input-stanza' rows="3"></textarea>
-
-          <label for="exampleInputEmail1" class="form-label">Edit Author Name:</label>
-          <input type="text" value="${this.author}" class="form-control" id='input-author' aria-describedby="emailHelp">
-        </div>
-        <div class="mb-3">
-            <select id="categories" class="categories" value="${this.category}"aria-label="Default select example">
-                <option selected> Please Select a Category: </option>
-                <option value= "1">Haiku</option>
-                <option value= "2">Free Verse</option>
-                <option value= "3">Sonnet</option>
-                <option value= "4">Acrostic</option>
-                <option value= "5">Villanelle</option>
-                <option value= "6">Limerick</option>
-                <option value= "7">Ode</option>
-                <option value= "8">Elegy</option>
-                <option value= "9">Ballad</option>
-                <option value= "10">Stanza</option>
-                <option value= "11">Epigram</option>
-                <option value= "12">Epitaph</option> 
-            </select>
-        </div>
+        <form id=${this.id}>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="${this.image_url}" class="card-img-top" alt="...">
+          <input id='input-url' type="text" placeholder="Change Image" value="${this.image_url}">
+            <div class="card-body">
+            <input id='input-title' type="text" placeholder="Change Title" value="${this.title}" >
+            <input id='input-genre' type="text" placeholder="Change Genre" value="${this.genre}" >
+            <input id='input-author' type="text" placeholder="Change Name" value="${this.author}" >
+            <textarea id='input-stanza' type="text" placeholder="Change Poem" value="${this.stanza}" ></textarea>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <select id="categories" class="categories" value="${this.category}"">
+                 <option selected> Edit Category: </option>
+                 <option value= "1">Haiku</option>
+                 <option value= "2">Free Verse</option>
+                 <option value= "3">Sonnet</option>
+                 <option value= "4">Acrostic</option>
+                 <option value= "5">Villanelle</option>
+                 <option value= "6">Limerick</option>
+                 <option value= "7">Ode</option>
+                 <option value= "8">Elegy</option>
+                 <option value= "9">Ballad</option>
+                 <option value= "10">Stanza</option>
+                 <option value= "11">Epigram</option>
+                 <option value= "12">Epitaph</option> 
+         </select>
+       </div>
         <button id="edit-button" name="submit" type="submit" class="btn btn-primary">Update Poem</button>
-      </form>
-</div>
+       </form>
         `
-        
     }
 
 
